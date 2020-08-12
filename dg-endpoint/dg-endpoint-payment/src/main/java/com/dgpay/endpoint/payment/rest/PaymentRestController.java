e8329d9d6dd756b6d01995a7c8b9931a8b20539b
Mohammadreza Momeni <mr.momeni@sadad.co.ir>
2020-08-12 14:03:03 +0430

    payment rest controller  implemented.

---
+++ dg-endpoint/dg-endpoint-payment/src/main/java/com/dgpay/endpoint/payment/rest/PaymentRestController.java
@@ -0,0 +1,18 @@
+package com.dgpay.endpoint.payment.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mohammadreza Momeni (mohamad7012@gmail.com)
+ * 8/12/2020
+ */
+@RestController
+@RequestMapping
+public class PaymentRestController {
+
+    // TODO: 8/12/2020 list
+    // TODO: 8/12/2020 add
+    // TODO: 8/12/2020 delete
+    // TODO: 8/12/2020 transaction
+}
